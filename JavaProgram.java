d639714e5e8db4a7c2efe727ad22f87d1008cdc6
hemalathauvaan <66261862+hemalathauvaan@users.noreply.github.com>
2020-07-24 16:28:23 +0530

    Update JavaProgram.java

--- JavaProgram.java
+++ JavaProgram.java
@@ -1,12 +1,12 @@
 class DisplayRes{  
-    void display(){
-		int[] a = {12, 30, 25, 18, 17}
-		System.Out.Println("Sample Array Program");
-		for(int i=0; i<5, i++)
-			System.Out.Println("a["+i+"] is " + a[i]);
+  	void display(){
+		int[] a = {12, 30, 25, 18, 17};
+		System.out.println("Sample Array Program");
+		for(int i=0; i<5; i++)
+			System.out.println("a["+i+"] is " + a[i]);
 	}  
 }  
-public class JavaProgram{
+public class TestEmployee{
 	public static void main(String args[]){
 		DisplayRes dr=new DisplayRes();
 		dr.display();  
